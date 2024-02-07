13de109ceedd98b844276f00d8fe5848f148a5b2
sauki futaki <utaki.sauki19@gmail.com>
2024-02-07 21:49:58 +0700

    entah keberepa

--- function.go
+++ function.go
@@ -2,10 +2,29 @@ package main
 
 import "fmt"
 
-func sayHello() {
-	fmt.Println("Hello World")
+//parameter
+func sayHello(firstName string, lastName string, age int) {
+	fmt.Println("Halo nama saya", firstName, lastName, "umur saya", age, "tahun")
+}
+
+//return value
+//func namaFunction(parameter) tipeDataReturn
+func gethello(p string) string {
+	return "Hallo " + p
+}
+
+//return value multiple
+//func namaFunction(parameter) (tipeDataReturn1, tipeDataReturn2)
+func getFullName() (string, string) {
+	return "John", "Doe"
 }
 
 func main() {
-	sayHello()
+
+	sayHello("John", "Doe", 20)
+
+	fmt.Println(gethello("John"))
+
+	firstName, lastName := getFullName()
+	fmt.Println(firstName, lastName)
 }
